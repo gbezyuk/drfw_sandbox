ca0fe7189080958de8fd2827abcfa9cb140fb354
Grigoriy Beziuk <gbezyuk@gmail.com>
2017-06-28 14:42:28 +0300

    bugfix

--- backend/bookshelf/serializers.py
+++ backend/bookshelf/serializers.py
@@ -25,7 +25,7 @@ class BookSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True)
     author_id = serializers.IntegerField()
     coauthor = AuthorSerializer(read_only=True, required=False)
-    coauthor_id = serializers.IntegerField(required=False)
+    coauthor_id = serializers.IntegerField(required=False, allow_null=True)
     publisher = PublisherSerializer(read_only=True)
     publisher_id = serializers.IntegerField()
 
@@ -50,6 +50,6 @@ class BookSerializer(serializers.ModelSerializer):
         publisher_id = validated_data.pop('publisher_id ')
         author = Author.objects.get(id=author_id)
         publisher = Publisher.objects.get(id=publisher_id)
-        coauthor = Author.objects.get(id=coauthor_id)
+        coauthor = Author.objects.get(id=coauthor_id) if coauthor_id else None
         book = Book.objects.create(author=author, coauthor=coauthor, publisher=publisher, **validated_data)
         return book
